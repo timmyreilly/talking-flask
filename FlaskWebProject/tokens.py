213179ac05af09d7c25bed7da50e95dfe1f04e5c
timmyreilly <timreilly@live.com>
2015-11-22 06:30:10 -0800

    removed credentials

--- FlaskWebProject/tokens.py
+++ FlaskWebProject/tokens.py
@@ -2,5 +2,5 @@
 #Search for Speech APIs from Azure Marketplace.
 #Use the subscription key as Client secret below.
 
-clientId = "Azure_2d404b0bcf-0264-4d44-B30f-910db87d736f"
-clientSecret = "e55a33d20e2b4d87b0e2c756f9b9ed0b"
+clientId = "Azure_your fance id"
+clientSecret = "32digits of secret"
